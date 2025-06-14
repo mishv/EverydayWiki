89b83dcba1b7abac5c0fc66aa82b17ea50d387b7
mishv <75266428+mishv@users.noreply.github.com>
2025-06-14 12:57:15 +0400

    pom updated

--- src/main/java/com/amazing/EverydayWiki/EverydayWikiApplication.java
+++ src/main/java/com/amazing/EverydayWiki/EverydayWikiApplication.java
@@ -1,5 +1,15 @@
-package com.amazing.EverydayWiki;
+// Dedicated to Steven Paul Jobs
+// RIP 1955 - 2011
+/*
 
+“There’s lots of ways to be as a person,
+and some people express their deep appreciation in different ways,
+but one of the ways that I believe people express their appreciation
+to the rest of humanity is to make something wonderful and put it out there.”
+
+*/
+
+package com.amazing.EverydayWiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,7 +17,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class EverydayWikiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EverydayWikiApplication.class, args);
 	}
